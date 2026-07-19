07e2f3b6d255d1a0493e4ad0e59475be3f36ea9d
RIYAD.RTB <riyadmallek1@gmail.com>
2026-07-19 15:14:15 +0100

    first commit

---
+++ sw.js
@@ -0,0 +1,46 @@
+var CACHE_NAME = 'mits-admin-v1';
+var PRECACHE_URLS = [
+  'admin.html',
+  'config.js'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(PRECACHE_URLS);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (names) {
+      return Promise.all(
+        names.map(function (name) {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      var fetchPromise = fetch(event.request).then(function (response) {
+        if (response && response.status === 200 && response.type === 'basic') {
+          var clone = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      }).catch(function () {
+        return cached;
+      });
+      return cached || fetchPromise;
+    })
+  );
+});
